b6cbf9c0fb8f924316ae4412907c44ae30f455f6
Hasnain Hossain <has9h2@gmail.com>
2018-10-24 02:07:45 +0000

    Classwork 6

--- script.js
+++ script.js
@@ -6,12 +6,48 @@ var products = [
   {name: "Old Ford Car Model", price: "46", image: "https://s3.amazonaws.com/mernbook/marketplace/Ford.jpg"},
   {name: "Storm Trooper Figurine", price: "23", image: "https://s3.amazonaws.com/mernbook/marketplace/stormtrooper-1995015_960_720.jpg"}
 ];
-var cart = {
-  items: [],
-  total: 0
-};
+var cart = localStorage.getItem('cart') 
+            ? JSON.parse(localStorage.getItem('cart')) 
+            : {
+            items: [],
+            total: 0
+          };
+
+localStorage.setItem('cart', JSON.stringify(cart));
 
 $(document).ready(function(){
+  products.forEach(function(product, index){
+    // console.log(index);
+    var colDiv = $('div').addClass('col-md-4');
+    var cardDiv = $('div').addClass('card');
+    $('#product-row').prepend(colDiv);
+  });
+  
+  // <div class="col-md-4">
+  //         <div class="card">
+  //           <img class="card-img-top" src="https://s3.amazonaws.com/mernbook/marketplace/tardis.png" alt="Card image cap">
+  //           <div class="card-body">
+  //             <h5 class="card-title">Tardis Figurine</h5>
+  //             <p class="card-text">$ 14</p>
+  //             <button class="btn btn-primary">Add to Cart</button>
+  //           </div>
+  //         </div>
+  //       </div>
+  
+  
+  $("#itemNo").text(cart.items.length);
+  
+  $("#showCartBtn").click(function(){
+    $("#cart").show();
+    $("#products").hide();
+    $("#showCartBtn").hide();
+  });
+  
+  $("#close").click(function(){
+    $("#cart").hide();
+    $("#products").show();
+    $("#showCartBtn").show();
+  });
   console.log("Start here");
   // Basic tasks
   // 1. Show / hide cart section on button click (Cart button / close cutton)
